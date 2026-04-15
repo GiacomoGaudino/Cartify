71eba5219b06fb006d803b100eb9742678a14736
GiacomoGaudino <giacomo.gaudino@yahoo.it>
2026-04-15 12:35:34 +0200

    Aggiungi i modelli Order e OrderItem con i campi fillable e le relazioni

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'customer_name',
+        'customer_email',
+        'shipping_address',
+        'total',
+        'status',
+    ];
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
